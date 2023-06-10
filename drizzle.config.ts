a3960ee32717f8162b7d1c76102c56444e4281e2
mac <mharrvic@gmail.com>
2023-06-10 23:42:15 +0800

    Update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,5 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/lib/db/schema.ts",
   out: "./src/lib/db/migrations",
-  connectionString: process.env.DATABASE_URL,
   breakpoints: true,
 } satisfies Config;
