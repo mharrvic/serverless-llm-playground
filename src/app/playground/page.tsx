b271c0508eb44293c2f49078c9ebe660174590b5
mac <mharrvic@gmail.com>
2023-06-13 01:00:32 +0800

    Update components path

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,5 +1,5 @@
 import { Send, Sparkles } from "lucide-react";
-import { Button } from "~/components/ui/button";
+import { Button } from "~/components/ui/common/button";
 
 export default function Playground() {
   return (
