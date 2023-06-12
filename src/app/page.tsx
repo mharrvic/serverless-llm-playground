07d9022d8d6c0adeb4ff28ffd6a179285eed12fb
mac <mharrvic@gmail.com>
2023-06-12 17:30:45 +0800

    Init model selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { Send, Sparkles } from "lucide-react";
+import { Code2, PlusCircle, Send, Settings, Sparkles } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import { ModelSelection } from "./playground/model-selection-combobox";
 
 export default function Home() {
   return (
@@ -17,7 +18,20 @@ export default function Home() {
           <div className="md:divide-x md:divide-y-0 divide-y border-t bg-zinc-50 flex flex-col md:flex-row h-full w-full overflow-y-hidden">
             <div className="flex md:min-w-[465px] md:flex-1 md:flex-grow w-full">
               <div className="py-6 px-4 lg:px-6 flex-grow w-full">
-                <div className="flex items-center justify-between pb-0.5"></div>
+                <div className="flex items-center justify-between pb-0.5">
+                  <ModelSelection />
+                  <div className="flex items-center ml-1 space-x-1">
+                    <Button variant="ghost">
+                      <PlusCircle className="h-5 w-5" />
+                    </Button>
+                    <Button variant="ghost">
+                      <Code2 className="h-5 w-5" />
+                    </Button>
+                    <Button variant="ghost">
+                      <Settings className="h-5 w-5" />
+                    </Button>
+                  </div>
+                </div>
                 <div className="w-full text-black bg-zinc-50 min-h-[200px] placeholder-zinc-400 h-full border-none focus:ring-0 focus:border-black pt-2 pb-16 px-0 rounded-lg">
                   <div className="prose prose-pre:bg-[#282c34] flex-1 prose-sm max-w-none w-full">
                     Results here
