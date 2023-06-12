5af14caca90791b99f8bbae807e6daf799dee529
mac <mharrvic@gmail.com>
2023-06-13 01:00:55 +0800

    Generate response

--- src/app/playground/generate.tsx
+++ src/app/playground/generate.tsx
@@ -1,59 +1,78 @@
 "use client";
 
-import {
-  Code2,
-  Loader2,
-  PlusCircle,
-  Send,
-  Settings,
-  Sparkles,
-} from "lucide-react";
+import { Loader2, Send, Sparkles } from "lucide-react";
 import React from "react";
-import { Button } from "~/components/ui/button";
+import { Button } from "~/components/ui/common/button";
+import LLMSettingsButton from "~/components/ui/llm-settings-btn";
+import { models } from "~/model-config";
 import { ModelSelection } from "./model-selection-combobox";
+
+const initModelState: { [key: string]: string } = {};
+
+models.forEach(({ endpoint }) => {
+  initModelState[endpoint] = "";
+});
 
 export default function Generate() {
   const [loading, setLoading] = React.useState(false);
   const [input, setInput] = React.useState("");
-  const [response, setResponse] = React.useState<String>("");
+  const [responses, setResponses] = React.useState(initModelState);
+
+  const onClear = () => {
+    setInput("");
+    setResponses(initModelState);
+  };
 
   const generateResponse = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setResponse("");
+    setResponses(initModelState);
     setLoading(true);
 
-    const response = await fetch(process.env.NEXT_PUBLIC_FALCON_LLM!, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_MODAL_KEY}`,
-      },
-      body: JSON.stringify({
-        prompt: input,
-      }),
+    const responses = await Promise.all(
+      models.map(async (llm) => {
+        return await fetch(llm.endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_MODAL_KEY}`,
+          },
+          body: JSON.stringify({
+            prompt: input,
+          }),
+        });
+      })
+    );
+
+    responses.map(async (response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      // This data is a ReadableStream
+      const data = response.body;
+      if (!data) {
+        return;
+      }
+
+      const reader = data.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        setLoading(true);
+        setResponses((prev) => ({
+          ...prev,
+          [response.url]: prev[response.url] + chunkValue,
+        }));
+      }
+
+      if (done) {
+        setLoading(false);
+      }
     });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    // This data is a ReadableStream
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      const chunkValue = decoder.decode(value);
-      setResponse((prev) => prev + chunkValue);
-    }
-    setLoading(false);
   };
 
   return (
@@ -73,35 +92,38 @@ export default function Generate() {
             </div>
           </div>
           <div className="md:divide-x md:divide-y-0 divide-y border-t bg-zinc-50 flex flex-col md:flex-row h-full w-full overflow-y-hidden">
-            <div className="flex md:min-w-[465px] md:flex-1 md:flex-grow w-full">
-              <div className="py-6 px-4 lg:px-6 flex-grow w-full">
-                <div className="flex items-center justify-between pb-0.5">
-                  <ModelSelection />
-                  <div className="flex items-center ml-1 space-x-1">
-                    <Button variant="ghost">
-                      <PlusCircle className="h-5 w-5" />
-                    </Button>
-                    <Button variant="ghost">
-                      <Code2 className="h-5 w-5" />
-                    </Button>
-                    <Button variant="ghost">
-                      <Settings className="h-5 w-5" />
-                    </Button>
+            {models.map((model) => {
+              return (
+                <div
+                  className="flex md:min-w-[465px] md:flex-1 md:flex-grow w-full"
+                  key={model.endpoint}
+                >
+                  <div className="py-6 px-4 lg:px-6 flex-grow w-full">
+                    <div className="flex items-center justify-between pb-0.5">
+                      <ModelSelection
+                        models={models}
+                        defaultValue={model.name}
+                        disabled
+                      />
+                      <LLMSettingsButton />
+                    </div>
+                    <div className="w-full text-black bg-zinc-50 min-h-[200px] placeholder-zinc-400 h-full border-none focus:ring-0 focus:border-black pt-2 pb-16 px-0 rounded-lg">
+                      <div className="prose prose-pre:bg-[#282c34] flex-1 prose-sm max-w-none w-full">
+                        {responses[model.endpoint]}
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="w-full text-black bg-zinc-50 min-h-[200px] placeholder-zinc-400 h-full border-none focus:ring-0 focus:border-black pt-2 pb-16 px-0 rounded-lg">
-                  <div className="prose prose-pre:bg-[#282c34] flex-1 prose-sm max-w-none w-full">
-                    {response}
-                  </div>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
       <div className="sticky bottom-0 left-0 right-0 w-full border-t h-[64px] -mt-16 bg-zinc-50 z-30 px-4 lg:px-6 flex items-center justify-between py-2 shadow">
         <div className="space-x-2 flex items-center">
-          <Button variant="outline">Clear</Button>
+          <Button variant="outline" onClick={onClear} disabled={loading}>
+            Clear
+          </Button>
         </div>
         <div>
           {loading ? (
