5aeeee86fc27da98c830153e239bdc1c0351949e
Mac Chic <mharrvic@gmail.com>
2023-12-30 12:02:35 +0800

    Add new models to model-config.ts

--- src/model-config.ts
+++ src/model-config.ts
@@ -5,19 +5,25 @@ export const models = [
   //   endpoint: `https://${MODAL_API_NAME}--stream-test-generate.modal.run/`,
   //   name: "stream-test-generate",
   // },
-  {
-    endpoint: `https://${MODAL_API_NAME}--example-falcon-gptq-generate.modal.run/`,
-    name: "falcon-40b-instruct",
-    link: "https://huggingface.co/TheBloke/falcon-40b-instruct-GPTQ",
-  },
+
   // {
   //   endpoint: `https://${MODAL_API_NAME}--open-llama-generate.modal.run/`,
   //   name: "open-llama",
   //   link: "https://github.com/openlm-research/open_llama",
   // },
   {
+    endpoint: `https://${MODAL_API_NAME}--example-mistral-vllm-inference-completion.modal.run/`,
+    name: "mistral-7B-Instruct-v0.1",
+    link: "https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.1",
+  },
+  {
     endpoint: `https://${MODAL_API_NAME}--example-vllm-mixtral-completion.modal.run/`,
     name: "mixtral-8x7b-instruct",
     link: "https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1",
+  },
+  {
+    endpoint: `https://${MODAL_API_NAME}--example-falcon-gptq-generate.modal.run/`,
+    name: "falcon-40b-instruct",
+    link: "https://huggingface.co/TheBloke/falcon-40b-instruct-GPTQ",
   },
 ];
