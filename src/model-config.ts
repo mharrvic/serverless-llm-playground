c0af8deef16a5816a6d70fe7cb8de6ab4aab0b04
Mac Chic <mharrvic@gmail.com>
2023-12-30 11:23:38 +0800

    Update model configurations

--- src/model-config.ts
+++ src/model-config.ts
@@ -1,16 +1,23 @@
 const MODAL_API_NAME = process.env.NEXT_PUBLIC_MODAL_API_NAME;
 
 export const models = [
-  {
-    endpoint: `https://${MODAL_API_NAME}--stream-test-generate.modal.run/`,
-    name: "stream-test-generate",
-  },
+  // {
+  //   endpoint: `https://${MODAL_API_NAME}--stream-test-generate.modal.run/`,
+  //   name: "stream-test-generate",
+  // },
   {
     endpoint: `https://${MODAL_API_NAME}--example-falcon-gptq-generate.modal.run/`,
     name: "falcon-40b-instruct",
+    link: "https://huggingface.co/TheBloke/falcon-40b-instruct-GPTQ",
   },
+  // {
+  //   endpoint: `https://${MODAL_API_NAME}--open-llama-generate.modal.run/`,
+  //   name: "open-llama",
+  //   link: "https://github.com/openlm-research/open_llama",
+  // },
   {
-    endpoint: `https://${MODAL_API_NAME}--open-llama-generate.modal.run/`,
-    name: "open-llama",
+    endpoint: `https://${MODAL_API_NAME}--example-vllm-mixtral-completion.modal.run/`,
+    name: "mixtral-8x7b-instruct",
+    link: "https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1",
   },
 ];
